97eacf44a3abc35a86d7611b397d748006b3a86f
Bruno Alves <brunovicentealves.ti@gmail.com>
2023-07-06 16:57:12 -0300

    import httpClientModules,FormsModule,ReactiveFormsModule

--- moments/src/app/app.module.ts
+++ moments/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -22,7 +24,10 @@ import { MomentFormComponent } from './components/moment-form/moment-form.compon
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
