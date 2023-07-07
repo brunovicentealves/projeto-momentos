b2a364bb326c66751e95277add21a3c009f023b4
Bruno Alves <brunovicentealves.ti@gmail.com>
2023-07-06 22:12:18 -0300

    validate form

--- moments/src/app/components/moment-form/moment-form.component.ts
+++ moments/src/app/components/moment-form/moment-form.component.ts
@@ -1,4 +1,5 @@
 import { Component,OnInit,Input } from '@angular/core';
+import {FormGroup,FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-moment-form',
@@ -8,11 +9,41 @@ import { Component,OnInit,Input } from '@angular/core';
 export class MomentFormComponent implements OnInit{
   @Input() btnText!:string
 
+  //linha 1 [formGroup]="momentForm"
+  momentForm!:FormGroup
+
+
   constructor(){
 
   }
 
   ngOnInit(): void {
-      
+      this.momentForm=new FormGroup({
+        id:new FormControl(''),
+        title:new FormControl('',[Validators.required]),
+        description:new FormControl('',[Validators.required]),
+        image:new FormControl(''),
+        
+      })
+  }
+
+  //linha 5 valida o html *ngIf="title.invalid && formDir.submitted"
+  get title (){
+    return this.momentForm.get('title')!;
+  }
+  //linha 12 valida html *ngIf="description.invalid && formDir.submitted"
+  get description (){
+    return this.momentForm.get('description')!;
+  }
+
+
+  submit(){
+    //valida se submit for invalido não deixa terminar submissão
+    if(this,this.momentForm.invalid){
+      return
+    }
+
+    console.log("enviar formulario");
+    
   }
 }
