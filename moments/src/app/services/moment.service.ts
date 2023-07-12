88141fa58dd2e9f4236afe63cecc675e4444fa7c
Bruno Alves <brunovicentealves.ti@gmail.com>
2023-07-11 23:15:08 -0300

    finishing at home

--- moments/src/app/services/moment.service.ts
+++ moments/src/app/services/moment.service.ts
@@ -5,6 +5,8 @@ import {Observable} from 'rxjs';
 
 import { Moment } from '../Moments';
 
+import { Reponse } from '../Response';
+
 import { environment } from '../environments/environment';
 
 @Injectable({
@@ -16,6 +18,11 @@ export class MomentService {
   private apiUrl = `${this.baseApiUrl}api/moments`
 
   constructor(private http:HttpClient) { }
+
+  getMoments():Observable<Reponse<Moment[]>>{
+
+    return this.http.get<Reponse<Moment[]>>(this.apiUrl);
+  }
 
   createMoment(formData:FormData):Observable<FormData>{
     return this.http.post<FormData>(this.apiUrl,formData);
